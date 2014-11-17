d8ba5720f2c36f7f348ca080bd86721fba2968a3
John Joseph Morgan <johnjosephmorgan@gmail.com>
2014-11-17 08:49:37 -0500

    added module for computing geometric mean of precisions using synonyms generated by word2vec

---
+++ log_synonym_precision.py
@@ -0,0 +1,27 @@
+from collections import Counter
+import math
+import sys
+
+from ngrams import ngrams
+from synonym_precisions import synonym_precisions
+
+
+def log_synonym_precision(corpus, model, history):
+    precisions = synonym_precisions(corpus, model, history)
+    weights = [1.0 / float(history)] * int(history)
+    log_precision = 0.0
+    for ww, pp in zip(weights, precisions):
+        if precisions[pp] > 0.0:
+            log_precision += ww * math.log(precisions[pp])
+
+    return log_precision
+
+if __name__ == '__main__':
+    if len(sys.argv) < 4:
+        print 'USAGE: log_synonym_precision.py CORPUS MODEL HISTORY'
+        exit()
+
+    corpus = sys.argv[1]
+    model = sys.argv[2]
+    history = int(sys.argv[3])
+    print log_synonym_precision(corpus, model, history)
