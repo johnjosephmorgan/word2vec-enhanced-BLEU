27b39d6872953f48d21e2b83bd086a211447b5bf
John Joseph Morgan <johnjosephmorgan@gmail.com>
2014-11-19 09:25:57 -0500

    added geometric mean version of bleu

---
+++ fall2014/single-reference/bleu/gmean_modified_precision.py
@@ -0,0 +1,19 @@
+import sys
+
+import numpy as np
+from scipy.stats import gmean
+from modified_precisions import modified_precisions
+
+
+def gmean_modified_precision(corpus, history):
+    precisions = modified_precisions(corpus, history)
+    return gmean(precisions.values())
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print 'USAGE: log_modified_precision.py CORPUS HISTORY'
+        exit()
+
+    corpus = sys.argv[1]
+    history = int(sys.argv[2])
+    print log_modified_precision(corpus, history)
