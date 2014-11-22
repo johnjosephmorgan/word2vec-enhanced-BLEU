03f277c63784a8d5c9955970406359783e70dfba
John Joseph Morgan <johnjosephmorgan@gmail.com>
2014-11-21 22:49:19 -0500

    added script to make ngrams with a counter indexed with tuples

--- fall2014/single-reference/vleu/ngram_counter.py
+++ fall2014/single-reference/vleu/ngram_counter.py
@@ -1,24 +1,22 @@
+from collections import Counter
 import sys
 
 
-def ngram_tuple(sentence, n):
-    """
-    Returns the ngrams from a given sentence for a given n.
-    """
+def ngram_counter(sentence, n):
     assert isinstance(sentence, list), "Sentences are lists, got %s: %s" \
         % (str(type(sentence)), str(sentence))
-    ngrams = tuple()
+    ngrams = Counter()
     for start in range(0, len(sentence) - n + 1):
-        ngrams = tuple(sentence[start:start + n])
+        ngrams[tuple(sentence[start:start + n])] += 1
 
     return ngrams
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print 'USAGE: ngram_tuple.py SENTENCE n'
+        print 'USAGE: ngram_counter.py SENTENCE n'
         exit()
     else:
         sentence = sys.argv[1].split()
         n = int(sys.argv[2])
-        out = ngram_tuple(sentence, n)
+        out = ngram_counter(sentence, n)
         print out
