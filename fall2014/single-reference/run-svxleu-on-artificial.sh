ab137bd4e28bb3be27a5a0ed5cfe8f0bb286b4e6
John Joseph Morgan <johnjosephmorgan@gmail.com>
2014-12-10 09:53:49 -0500

    added script to run svxleu on artificial data

---
+++ fall2014/single-reference/run-svxleu-on-artificial.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -x
+
+mkdir -p scores logs
+
+rm xleu_count_syns.log
+
+#generate some very simple sentences
+python scripts/my_eval.py \
+> data/gen.txt
+
+#split the list of simple sentences
+#1 sublist will serve as candidates and 1 as references
+scripts/gen2corpus.pl \
+data/gen.txt
+
+shuf ./cands.txt > data/cands.txt
+shuf ./refs.txt > data/refs.txt
+
+#paste the candidates and references into 1 file
+# we will use the cdec format for an evaluation corpus
+# CANDIDATESENTENCE ||| REFERENCES 
+scripts/paste-files.pl \
+data/cands.txt \
+data/refs.txt \
+> data/eval_artificial.txt
+
+mkdir -p results
+
+python \
+svxleu/svxleu.py \
+-C data/eval_artificial.txt \
+-V ./GoogleNews-vectors-negative300.bin.gz \
+> scores/svxleu-artificial.txt
+
+
+scripts/multi-bleu.perl \
+data/refs.txt \
+< data/cands.txt \
+> scores/multibleu-artificial.txt
+
+rm svxleu_candidate_reference_similarity.log
+
+echo "candidate	reference	similarity" > svxleu_candidate_reference_similarity.log
+cat ./xleu_count_syns.log \
+| grep root \
+| perl -e \
+"while (<>) {s/INFO:root://;s/[']//g;s/\[//g;s/\]//g;s/\(//g;s/\)//g;s/,//g;print;}" \
+| sort -u \
+>> svxleu_candidate_reference_similarity.log
+
+
+mv svxleu_candidate_reference_similarity.log logs/
