6d8c50be85e9b645e9fd08645abe5c30c901a211
John Joseph Morgan <johnjosephmorgan@gmail.com>
2015-01-10 13:56:33 -0500

    minor changes to svxleu.py

--- fall2014/single-reference/svxleu/svxleu.py
+++ fall2014/single-reference/svxleu/svxleu.py
@@ -1,14 +1,14 @@
 import argparse
-import logging
 
 from brevity_penalty import brevity_penalty
 from gensim.models.word2vec import Word2Vec as w2v
 from gmean_synonym_precision import gmean_synonym_precision
 
 
-def xleu(corpus, model, n, m, threshold):
-    print m, 'xleu	',
-    return gmean_synonym_precision(corpus, model, n, m, threshold) * brevity_penalty(corpus)
+def xleu(corpus, model, method, threshold, precision=4):
+    print method, 'bleu	',
+    return gmean_synonym_precision(corpus, model, precision, method, threshold)\
+        * brevity_penalty(corpus)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -22,11 +22,7 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--threshold', type=float,
                         default=0.7, help='Similarity threshold. Only ngram pairs with similarity exceeding this threshold will be counted.')
     args = parser.parse_args()
-    logging.basicConfig(
-        filename='xleu.log', level=logging.DEBUG)
-
     model = w2v.load_word2vec_format(fname=args.vectors, binary=True)
     for ii in ['x', 'v', 's']:
-        xb = xleu(args.corpus, model, args.precision, ii, args.threshold)
+        xb = xleu(args.corpus, model, ii, args.threshold, args.precision)
         print xb
-
